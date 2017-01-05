a73ca19fed9ab4a6cf214693f08d2eec5696341a
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-01-05 16:00:11 -0600

    Add new auth reducer.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import auth from './auth';
 import track from './track';
 
 export default combineReducers({
 	// using this helper func to combine all reducers into another easy interface
 	// This func helps share the state between reducers instead of having one reducer return the entire state
+	auth,
 	track,
 	routing: routerReducer
 });
