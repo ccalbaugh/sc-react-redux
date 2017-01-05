8191582410f2d3369618b99945a25b56c1223469
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-01-05 09:48:37 -0600

    Add middleware to store.

--- src/stores/configureStore.js
+++ src/stores/configureStore.js
@@ -1,6 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/index';
 
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
 export default function configureStore(initialState) {
-	return createStore(rootReducer, initialState);
+	return createStoreWithMiddleware(rootReducer, initialState);
 }
