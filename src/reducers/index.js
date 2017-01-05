7a1d53e45bc8841d51a35b8ad2ca2903186cffd1
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-01-05 11:55:28 -0600

    Wired store to sync with browser history.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
 import track from './track';
 
 export default combineReducers({
 	// using this helper func to combine all reducers into another easy interface
 	// This func helps share the state between reducers instead of having one reducer return the entire state
-	track
+	track,
+	routing: routerReducer
 });
