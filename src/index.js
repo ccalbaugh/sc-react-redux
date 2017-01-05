7bf29f418b03ea9ff01566541b0920b9f9137849
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-01-05 11:03:42 -0600

    Add soundcloud authorization.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+import sc from 'soundcloud';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './stores/configureStore.js';
 import * as actions from './actions';
 import Stream from './components/Stream';
+import { CLIENT_ID, REDIRECT_URI } from './constants/auth.js';
+
+sc.initialize({ client_id: CLIENT_ID, redirect_url: REDIRECT_URI });
 
 const tracks = [
   {
