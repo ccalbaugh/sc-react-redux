9eaf8374ecbc4dfa882556b30c80fb4ad3f823e4
Caden Albaugh <CadenAlbaugh@gmail.com>
2017-01-05 08:06:07 -0600

    Add testing suite and fix syntax error in Stream component

--- src/components/Stream.js
+++ src/components/Stream.js
@@ -1,19 +1,15 @@
 import React from 'react';
 
 function Stream({ tracks = [] }) {
-	render() {
-		const { tracks = [] } = this.props;
-
-		return (
-			<div>
-				{
-					tracks.map((track, key) => {
-						return <div className="track" key={key}>{track.title}</div>;
-					})
-				}
-			</div>
-		);
-	}
+	return (
+		<div>
+			{
+				tracks.map((track, key) => {
+					return <div className="track" key={key}>{track.title}</div>;
+				})
+			}
+		</div>
+	);
 }
 
 export default Stream;
